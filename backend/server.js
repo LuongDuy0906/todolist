61016f179c4f572f886b03406646ad0a6a2e634a
LuongDuy0906 <luognducduy0906@gmail.com>
2025-11-25 14:52:00 +0700

    First commit from node todolist

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import notesRoutes from "./src/routes/notesRoutes.js"
+import { connectDB } from './src/config/db.js';
+import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("server is start on port", PORT);
+    });
+});
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import axios from 'axios';
+
+const HomePage = () => {
+  const [ isRateLimited, setIsRateLimited ] = useState(false);
+  const [ notes, setNotes ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+  
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+          const res = await axios.get("http://localhost:5001/api/notes");
+          console.log(res.data);
+      } catch (error) {
+        console.log("Error to fetch data", error);
+      }
+    }
+
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className='min-h-screen'>
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  )
+}
+
+export default HomePage
