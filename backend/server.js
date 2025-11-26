e9209228867919bde2ccb4948203a467b9783786
LuongDuy0906 <luongducduy0906@gmail.com>
2025-11-26 10:16:10 +0700

    update home page in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from "./src/routes/notesRoutes.js"
 import { connectDB } from './src/config/db.js';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 app.use(express.json());
 app.use(rateLimiter);
+
 
 app.use("/api/notes", notesRoutes);
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
 import axios from 'axios';
+import toast from 'react-hot-toast';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
   const [ isRateLimited, setIsRateLimited ] = useState(false);
@@ -12,9 +14,19 @@ const HomePage = () => {
     const fetchNotes = async () => {
       try {
           const res = await axios.get("http://localhost:5001/api/notes");
-          console.log(res.data);
+          setNotes(res.data);
+
+          setIsRateLimited(false);
       } catch (error) {
         console.log("Error to fetch data", error);
+        if(error.response?.status == 429){
+          setIsRateLimited(true);
+        }
+        else{
+          toast.error("Failed to load data");
+        }
+      } finally{
+        setLoading(false);
       }
     }
 
@@ -26,6 +38,18 @@ const HomePage = () => {
       <Navbar />
 
       {isRateLimited && <RateLimitedUI />}
+
+      <div className='max-w-7xl mx-auto  p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note}/>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
